b8500e5800583d0757a14306d86477ff22ca08f8
Rohan van Wijk <rohan_10@live.nl>
2020-09-11 13:22:22 +0200

    styled error page

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,40 +1,36 @@
 <template>
-  <v-app dark>
-    <h1 v-if="error.statusCode === 404">
-      {{ pageNotFound }}
-    </h1>
-    <h1 v-else>
-      {{ otherError }}
-    </h1>
-    <NuxtLink to="/">
-      Home page
-    </NuxtLink>
+  <v-app>
+    <v-alert type="error">
+      <h1 v-if="error.statusCode === 404">{{ pageNotFound }}</h1>
+      <h1 v-else>{{ otherError }}</h1>
+    </v-alert>
+    <v-btn nuxt="true" to="/">Home page</v-btn>
   </v-app>
 </template>
 
 <script>
 export default {
-  layout: 'empty',
+  layout: "mws",
   props: {
     error: {
       type: Object,
-      default: null
-    }
+      default: null,
+    },
   },
-  data () {
+  data() {
     return {
-      pageNotFound: '404 Not Found',
-      otherError: 'An error occurred'
-    }
+      pageNotFound: "404 Not Found",
+      otherError: "An error occurred",
+    };
   },
-  head () {
+  head() {
     const title =
-      this.error.statusCode === 404 ? this.pageNotFound : this.otherError
+      this.error.statusCode === 404 ? this.pageNotFound : this.otherError;
     return {
-      title
-    }
-  }
-}
+      title,
+    };
+  },
+};
 </script>
 
 <style scoped>
