5fdc37f20af4be4a59035b6dbd58c33841d600c8
Rohan van Wijk <rohan_10@live.nl>
2020-10-22 16:04:04 +0200

    added interface.

--- components/customer/ICustomer.ts
+++ components/customer/ICustomer.ts
@@ -1,23 +1,21 @@
 export default interface ICustomer {
+    address: string
+    city: string
+    company: string
+    country: string
+    creation_date: string
+    currency: string
+    email_address: string
+    email_unsubscribed:false
+    email_verified: boolean
+    forgot_password_date: string
     id: number
     name: string
-    username: string
-    email: string
-    address: {
-        street: string
-        suite: string
-        city: string
-        zipcode: string
-        geo: {
-            lat: string
-            lng: string
-        }
-    }
-    phone: string
-    website: string
-    company: {
-        name: string
-        catchPhrase: string
-        bs: string
-    }
+    other_club_notifs: boolean
+    payment_verified: boolean
+    phone_number: string
+    phone_verified: boolean
+    postal_code: string
+    recurring: boolean
+    status: boolean
 }
