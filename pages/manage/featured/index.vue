2c1149c011e6228cd3ed11e505aead4eee5dbbce
pedro fernandes <thephfernandes@gmail.com>
2020-10-30 11:14:03 +0100

    implements draggable slider image management and their store

---
+++ pages/manage/featured/index.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+    This is where the magic happens!
+        <v-data-table>
+            <template v-slot:body="props">
+                <draggable :list="props.items" tag="tbody">
+                    <tr v-for="(item, index) in props.items" :key="index"></tr>
+                    <td>{{ item.home_team }}</td>
+                    <td>{{ item.away_team }}</td>
+                    <td>{{ item.match_date }}</td>
+                    <td>{{ item.finished }}</td>
+                </draggable>
+            </template>
+        </v-data-table>
+    </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "nuxt-property-decorator";
+
+@Component
+export default class Index extends Vue {
+  layout() {
+    return "mws";
+  }
+};
+</script>
+
+<style>
+</style>
