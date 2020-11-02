d09f8bcf936c5cdf926ee0a63487019afbbb4fb3
Rohan van Wijk <rohan_10@live.nl>
2020-11-02 09:02:06 +0100

    Added getters to store.

--- store/customers.js
+++ store/customers.js
@@ -2,7 +2,20 @@ import customersData from '~/assets/data/customers.json';
 
 export const state = () => ({
     list: []
-})
+});
+
+export const getter = {
+    customers: (state) => {
+        return state.list;
+    },
+    getRecurring: (state) => {
+        return state.list.filter((c) => c.recurring)
+    },
+    totalCustomers: (state) => {
+        if (state.list.length == 0) return 0;
+        return state.list.length;
+    }
+};
 
 export const mutations = {
     fill(state, payload) {
@@ -12,12 +25,12 @@ export const mutations = {
         state.list.push(user);
     },
     remove(state, { user }) {
-        state.list.remove(user);
+        state.list.remove();
     }
-}
+};
 
 export const actions = {
     getAll({ commit }) {
         commit('fill', customersData);
     }
-}
+};
