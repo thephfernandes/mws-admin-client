7a1e8b7f9acd118dc3df876b4af3e6b27936792d
Rohan van Wijk <rohan_10@live.nl>
2020-11-04 13:34:36 +0100

    Added customer class.

---
+++ models/customer.ts
@@ -0,0 +1,37 @@
+export class Customer {
+    public id: number = 1100;
+    public name: string = '';
+    public email_address: string = '';
+    public phone_number: string = '';
+    public country: string = '';
+    public postal_code: string = '';
+    public city: string = '';
+    public address: string = '';
+    public company: string = '';
+    public creation_date: string = '';
+    public currency: string = '';
+    public forgot_password_date: string = '';
+    public email_unsubscribed:false = false;
+    public email_verified: boolean = false;
+    public other_club_notifs: boolean = false;
+    public payment_verified: boolean = false;
+    public phone_verified: boolean = false;
+    public recurring: boolean = false;
+    public status: boolean = false;
+
+    constructor() {
+        this.setId();
+        this.setDate();
+    }
+
+    private setId(): void {
+        const date = new Date();
+        this.id = date.getTime();
+    }
+
+    private setDate(): void {
+        const today = new Date().toDateString()
+        this.creation_date = today;
+        this.forgot_password_date = today;
+    }
+}
