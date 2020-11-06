be0210abdf85ecce138916548d733038dbbda13d
Rohan van Wijk <rohan_10@live.nl>
2020-11-06 13:22:56 +0100

    added leaderbord datatable to customer page.

--- pages/customers/index.vue
+++ pages/customers/index.vue
@@ -14,6 +14,11 @@
     </v-row>
     <v-row>
       <v-col>
+        <leader-bord />
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col>
         <DataTable :customers="customers" />
       </v-col>
     </v-row>
@@ -24,12 +29,14 @@
 import highLightCard from "~/components/shared/highlight-card.vue";
 import Actions from "~/components/customer/actions.vue";
 import DataTable from "~/components/customer/datatable.vue";
+import Leaderbord from "~/components/customer/leaderboard.vue";
 import {Component, Vue} from "nuxt-property-decorator";
 import { mapGetters } from "vuex";
 
 @Component({
   components: {
     'highlight-card': highLightCard,
+    'leader-bord': Leaderbord,
     Actions,
     DataTable,
   },
