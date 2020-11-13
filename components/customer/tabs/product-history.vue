b804a88737cff87e9ee66bb2ce89b9d32ab3f585
Rohan van Wijk <rohan_10@live.nl>
2020-11-13 13:15:05 +0100

    Added product history tab.

---
+++ components/customer/tabs/product-history.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <v-card-title>Product History</v-card-title>
+        <v-divider />
+        <v-card-title>Purchase history</v-card-title>
+        <v-card-title>Bids</v-card-title>
+        <v-card-title>Favourite items</v-card-title>
+    </div>
+</template>
+<script lang="ts">
+    import { Vue, Component } from "nuxt-property-decorator";
+
+    @Component
+    export default class ProductHistoryComponent extends Vue {
+        name(): string {
+            return 'product-history-component';
+        }
+    }
+</script>
