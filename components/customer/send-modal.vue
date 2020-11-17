dc9c5e07d49309552be78d2f0c40e7db696cc5d5
Rohan van Wijk <rohan_10@live.nl>
2020-11-17 10:15:30 +0100

    Added user feedback.

--- components/customer/send-modal.vue
+++ components/customer/send-modal.vue
@@ -13,14 +13,26 @@
             </v-card-subtitle>
             <v-row>
                 <v-col>
-                    <v-btn>Send guide</v-btn>
+                    <v-btn @click="sent = true">
+                        <v-icon class="mr-2">mdi-newspaper-variant-outline</v-icon>
+                        Send guide
+                    </v-btn>
                 </v-col>
                 <v-col>
-                    <v-btn>Send payment reminder</v-btn>
-                    <v-btn class="mt-4">Send address reminder</v-btn>
+                    <v-btn @click="sent = true">
+                        <v-icon class="mr-2">mdi-credit-card-clock-outline</v-icon>
+                        Send payment reminder
+                    </v-btn>
+                    <v-btn class="mt-4" @click="sent = true">
+                        <v-icon class="mr-2">mdi-google-maps</v-icon>
+                        Send address reminder
+                    </v-btn>
                 </v-col>
             </v-row>
         </v-card-text>
+        <v-snackbar v-model="sent" color="green">
+            Email has been sent to {{customer.email_address}}
+        </v-snackbar>
     </v-card>
 </template>
 <script lang="ts">
@@ -29,6 +41,7 @@
 
     @Component
     export default class SendModalComponent extends Vue {
+        private sent: boolean = false;
         @Prop({ type: Object, required: true }) readonly customer!: Customer;
 
         name(): string {
