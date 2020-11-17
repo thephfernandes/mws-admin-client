2676036cb16cd8fcadb1740a2654166c4f6eb9a3
Rohan van Wijk <rohan_10@live.nl>
2020-11-17 14:57:28 +0100

    removed localvue

--- test/customer/datatable.spec.js
+++ test/customer/datatable.spec.js
@@ -1,30 +1,26 @@
-import { shallowMount, createLocalVue } from '@vue/test-utils'
+import { shallowMount } from '@vue/test-utils'
 import DataTable from '@/components/customer/datatable.vue'
 import Vuetify from 'vuetify'
+import Vue from 'vue'
 import CustomersData from '@/assets/data/customers.json'
 
+Vue.use(Vuetify);
+
 describe('datatable.vue', () => {
-    let wrapper;
+    let vuetify;
 
     beforeEach(() => {
-        const localVue = createLocalVue();
-        localVue.use(Vuetify);
-        wrapper = shallowMount(DataTable, {
-            localVue,
-            propsData: {
-                customers: CustomersData
-            }
-        });
+        vuetify = new Vuetify();
+
     });
 
     test('is a Vue instance', () => {
-        let vuetify = new Vuetify();
         const wrapper = shallowMount(DataTable, {
             vuetify,
             propsData: {
                 customers: CustomersData
             }
         });
-        expect(wrapper.isVueInstance()).toBe(true);
+        expect(wrapper.vm).toBeTruthy();
     });
 });
