b6064995d6866198f5912f392ec3a48fedbf4450
Rohan van Wijk <rohan_10@live.nl>
2020-11-18 09:38:06 +0100

    Added test existance.

--- test/customer/datatable.spec.js
+++ test/customer/datatable.spec.js
@@ -1,4 +1,4 @@
-import { shallowMount } from '@vue/test-utils'
+import { shallowMount, mount } from '@vue/test-utils'
 import DataTable from '@/components/customer/datatable.vue'
 import Vuetify from 'vuetify'
 import Vue from 'vue'
@@ -14,13 +14,57 @@ describe('datatable.vue', () => {
 
     });
 
-    test('is a Vue instance', () => {
+    test('Component renders', () => {
         const wrapper = shallowMount(DataTable, {
             vuetify,
             propsData: {
                 customers: CustomersData
             }
         });
-        expect(wrapper.vm).toBeTruthy();
+        expect(wrapper.exists()).toBe(true);
+    });
+
+    test('Renders reset filters button',   () => {
+        const wrapper = shallowMount(DataTable, {
+            vuetify,
+            propsData: {
+                customers: CustomersData
+            }
+        });
+        const btnResetFilters =  wrapper.find('#test-resetFilters');
+        expect(btnResetFilters.exists()).toBe(true);
+    });
+
+    test('Renders new customer button', () => {
+        const wrapper = shallowMount(DataTable, {
+            vuetify,
+            propsData: {
+                customers: CustomersData
+            }
+        });
+        const btnNewCustomer =  wrapper.find('#test-newCustomer');
+        expect(btnNewCustomer.exists()).toBe(true);
+    });
+
+    test('Renders country select', () => {
+        const wrapper = shallowMount(DataTable, {
+            vuetify,
+            propsData: {
+                customers: CustomersData
+            }
+        });
+        const countrySelect =  wrapper.find('#test-countrySelect');
+        expect(countrySelect.exists()).toBe(true);
+    });
+
+    test('Renders search field', () => {
+        const wrapper = shallowMount(DataTable, {
+            vuetify,
+            propsData: {
+                customers: CustomersData
+            }
+        });
+        const searchField =  wrapper.find('#test-search');
+        expect(searchField.exists()).toBe(true);
     });
 });
