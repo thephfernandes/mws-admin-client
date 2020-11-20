8042a0a95f33430bae5b6ddfa927227b348f474c
Rohan van Wijk <rohan_10@live.nl>
2020-11-20 16:09:03 +0100

    Created order index file.

---
+++ pages/orders/index.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h1>Orders</h1>
+    </div>
+</template>
+<script lang="ts">
+    import {Component, Vue} from "nuxt-property-decorator";
+
+    @Component
+    export default class IndexOrders extends Vue {
+        layout(): string {
+            return 'mws';
+        }
+    }
+</script>
