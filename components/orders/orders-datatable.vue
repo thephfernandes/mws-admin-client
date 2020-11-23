6e3069284b842af435e0e3304b19ecad584bbb60
Rohan van Wijk <rohan_10@live.nl>
2020-11-23 12:24:43 +0100

    Add Datatable orders.

---
+++ components/orders/orders-datatable.vue
@@ -0,0 +1,93 @@
+<template>
+    <div class="orders-data">
+        <v-data-table :items="orders" :headers="headers">
+            <template v-slot:item.OrderID="{ item }">
+                <nuxt-link :to="`/orders/${item.OrderID}`" class="link">{{item.OrderID}}</nuxt-link>
+            </template>
+            <template v-slot:item.MatchID="{ item }">
+                <nuxt-link :to="`/events/${item.MatchID}`" class="link">{{item.MatchID}}</nuxt-link>
+            </template>
+            <template v-slot:item.PlayerName="{ item }">
+                <nuxt-link :to="`/products/${item.PlayerID}`" class="link">{{item.PlayerName}}</nuxt-link>
+            </template>
+            <template v-slot:item.UserMail="{ item }">
+                <nuxt-link :to="`/customers/${item.UserID}`" class="link">{{item.UserMail}}</nuxt-link>
+            </template>
+            <template v-slot:item.UserMail="{ item }">
+                <nuxt-link :to="`/customers/${item.UserID}`" class="link">{{item.UserMail}}</nuxt-link>
+            </template>
+            <template v-slot:item.OrderCreationDate="{ item }">
+                {{ formatDate(item.OrderCreationDate) }}
+            </template>
+        </v-data-table>
+    </div>
+</template>
+<script lang="ts">
+    import { Component, Vue, Prop } from "nuxt-property-decorator";
+    import { IOrder } from "../../interfaces/IOrder";
+    import Datatable from "~/components/customer/datatable.vue";
+    @Component({
+        components: {Datatable}
+    })
+    export default class OrdersTableComponent extends Vue {
+
+        @Prop({ type: Array, required: true }) readonly orders!: IOrder[];
+
+
+        name(): string {
+            return 'orders-table-component'
+        }
+
+        formatDate(string: string) {
+            const date = new Date(string);
+            const options = {
+                year: 'numeric', month: 'numeric', day: 'numeric',
+                hour: 'numeric', minute: 'numeric',
+                hour12: false,
+                timeZone: 'Europe/Amsterdam'
+            };
+            return new Intl.DateTimeFormat('nl-NL', options).format(date);
+        }
+
+        get headers() {
+            return [
+                {
+                    text: 'Id',
+                    value: 'OrderID'
+                },
+                {
+                    text: 'Status'
+                },
+                {
+                    text: 'Actions',
+                    divider: true
+                },
+                {
+                    text: 'Certificate #',
+                    value: 'OrderCertificateNumber'
+                },
+                {
+                    text: 'MatchId',
+                    value: 'MatchID'
+                },
+                {
+                    text: 'Player',
+                    value: 'PlayerName'
+                },
+                {
+                    text: 'Customer',
+                    value: 'UserMail'
+                },
+                {
+                    text: 'Order date',
+                    value: 'OrderCreationDate'
+                }
+            ]
+        }
+    }
+</script>
+<style lang="scss" scoped>
+    .link {
+        text-decoration: none;
+    }
+</style>
