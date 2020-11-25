2fb15bd7e59dc2d2d191047bd5579eeab5667266
Rohan van Wijk <rohan_10@live.nl>
2020-11-25 13:57:59 +0100

    added payment methods enum.

---
+++ enums/paymentMethod.ts
@@ -0,0 +1,14 @@
+export enum PaymentMethods {
+    Visa,
+    Mastercard,
+    iDeal,
+    Paypal,
+    Klarna,
+    Sofort,
+    Alipay,
+    WeChatPay,
+    BCMC,
+    BankTransferNL,
+    BankTransferUK
+
+}
