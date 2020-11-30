deecbfa4e4e221feb7f2a5298b477e13c454e69a
Rohan van Wijk <rohan_10@live.nl>
2020-11-30 20:57:56 +0100

    use flexbox

--- pages/login/index.vue
+++ pages/login/index.vue
@@ -1,46 +1,52 @@
 <template>
-  <v-card raised outlined tile dark class="login" max-width="600px">
-    <v-card-text>
-      <v-img :src="require('~/assets/logo-shirt.png')" contain />
-      <v-form class="login-form">
-        <v-row justify="center">
-          <v-col cols="8">
-            <v-alert color="red" v-if="errorMessage">{{errorMessage}}</v-alert>
-          </v-col>
-          <v-col cols="8">
-            <v-text-field
-                    prepend-icon="mdi-account"
-                    label="Username"
-                    v-model="username"
-                    :rules="[rules.required]"
-            />
-          </v-col>
-        </v-row>
-        <v-row justify="center">
-          <v-col cols="8">
-            <v-text-field
-                    prepend-icon="mdi-lock"
-                    label="Password"
-                    v-model="password"
-                    :append-icon="show ? 'mdi-eye' : 'mdi-eye-off'"
-                    :rules="[rules.required, rules.min]"
-                    :type="show ? 'text' : 'password'"
-                    @click:append="show = !show"
-            />
-          </v-col>
-        </v-row>
-        <v-row justify="center">
-          <v-col cols="8">
-            <v-card-actions>
-              <v-btn dark @click="login" block>
-                Login
-              </v-btn>
-            </v-card-actions>
-          </v-col>
-        </v-row>
-      </v-form>
-    </v-card-text>
-  </v-card>
+  <div>
+    <v-row justify="center" align="center">
+      <v-col cols="12" md="8">
+        <v-card raised outlined tile dark>
+          <v-card-text>
+            <v-img :src="require('~/assets/logo-shirt.png')" contain />
+            <v-form class="login-form">
+              <v-row justify="center">
+                <v-col cols="8">
+                  <v-alert color="red" v-if="errorMessage">{{errorMessage}}</v-alert>
+                </v-col>
+                <v-col cols="8">
+                  <v-text-field
+                          prepend-icon="mdi-account"
+                          label="Username"
+                          v-model="username"
+                          :rules="[rules.required]"
+                  />
+                </v-col>
+              </v-row>
+              <v-row justify="center">
+                <v-col cols="8">
+                  <v-text-field
+                          prepend-icon="mdi-lock"
+                          label="Password"
+                          v-model="password"
+                          :append-icon="show ? 'mdi-eye' : 'mdi-eye-off'"
+                          :rules="[rules.required, rules.min]"
+                          :type="show ? 'text' : 'password'"
+                          @click:append="show = !show"
+                  />
+                </v-col>
+              </v-row>
+              <v-row justify="center">
+                <v-col cols="8">
+                  <v-card-actions>
+                    <v-btn dark @click="login" block>
+                      Login
+                    </v-btn>
+                  </v-card-actions>
+                </v-col>
+              </v-row>
+            </v-form>
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
+  </div>
 </template>
 
 <script lang="ts">
@@ -88,11 +94,3 @@ export default class Index extends Vue {
   }
 }
 </script>
-<style scoped>
-.login {
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-}
-</style>
