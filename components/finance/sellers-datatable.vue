0e7a2c3c4a9fc2158e037f18207eeff3bf39fcb1
Rohan van Wijk <rohan_10@live.nl>
2020-12-03 12:38:32 +0100

    Added sellers data table.

---
+++ components/finance/sellers-datatable.vue
@@ -0,0 +1,94 @@
+<template>
+    <div>
+        <v-data-table
+            :items="sellers"
+            :headers="headers"
+            :footer-props="footerPropsOptions"
+        >
+            <template v-slot:top>
+                <v-toolbar flat>
+                    <v-toolbar-title>All sellers</v-toolbar-title>
+                    <v-spacer></v-spacer>
+                    <nuxt-link to="/finance/sellers/add" class="link">
+                        <v-btn color="success">
+                            <v-icon class="mr-2">mdi-plus</v-icon>
+                            New seller
+                        </v-btn>
+                    </nuxt-link>
+                </v-toolbar>
+            </template>
+            <template v-slot:item.id="{item}">
+                <nuxt-link :to="`/finance/sellers/${item.id}`" class="link">{{item.id}}</nuxt-link>
+            </template>
+            <template v-slot:item.email="{item}">
+                <a :href="`mailto:${item.email}`" class="link">{{item.email}}</a>
+            </template>
+            <template v-slot:item.website="{item}">
+                <a :href="`https://${item.website}`" class="link">{{item.website}}</a>
+            </template>
+        </v-data-table>
+    </div>
+</template>
+<script lang="ts">
+import {Component, Vue, Prop} from "nuxt-property-decorator";
+import {ISeller} from "~/interfaces/ISeller";
+
+@Component
+export default class SellersDatatableComponent extends Vue {
+    @Prop({ type: Array, required: true }) readonly sellers!: ISeller[];
+    footerPropsOptions = {
+        'items-per-page-options': [5, 10, 25, 50]
+    };
+
+    get headers() {
+        return [
+            {
+                text: 'Id',
+                value: 'id'
+            },
+            {
+                text: 'Next invoiceId',
+                value: 'nextInvoiceId',
+                divider: true
+            },
+            {
+                text: 'Invoice Prefix',
+                value: 'invoiceIdPrefix'
+            },
+            {
+                text: 'Name',
+                value: 'name'
+            },
+            {
+                text: 'VAT #',
+                value: 'vatNumber'
+            },
+            {
+                text: 'Address 1',
+                value: 'address1'
+            },
+            {
+                text: 'Address 2',
+                value: 'address2'
+            },
+            {
+                text: 'Country',
+                value: 'country'
+            },
+            {
+                text: 'Email',
+                value: 'email'
+            },
+            {
+                text: 'Website',
+                value: 'website'
+            }
+        ];
+    }
+}
+</script>
+<style lang="scss" scoped>
+.link {
+    text-decoration: none;
+}
+</style>
