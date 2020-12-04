4a41e097256b3b868f690493dbe10b85b9882c78
Rohan van Wijk <rohan_10@live.nl>
2020-12-04 15:54:19 +0100

    add overview invoice page.

---
+++ pages/finance/invoices/index.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <h1>Invoices</h1>
+        <invoices-datatable />
+    </div>
+</template>
+<script lang="ts">
+    import {Component, Vue} from "nuxt-property-decorator";
+
+    @Component
+    export default class InvoicesPage extends Vue {
+        layout(): string {
+            return 'mws';
+        }
+    }
+</script>
