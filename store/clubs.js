61d93ce872b58776781427c03080f78aec16aa88
Rohan van Wijk <rohan_10@live.nl>
2020-12-04 14:38:32 +0100

    updated clubs store.

--- store/clubs.js
+++ store/clubs.js
@@ -1,7 +1,8 @@
 import Clubs from "~/assets/data/clubs.json";
 
 export const state = () => ({
-    clubs: []
+    clubs: [],
+    invoiceAgreements: []
 });
 
 export const getters = {
@@ -10,12 +11,21 @@ export const getters = {
     },
     getClub: (state) => (id) => {
         return state.clubs.find((c) => c.id === id);
+    },
+    getInvoiceAgreements(state) {
+        return state.invoiceAgreements;
+    },
+    getInvoiceAgreement: (state) => (id) => {
+        return state.invoiceAgreements.find((i) => i.id === id);
     }
 };
 
 export const mutations = {
     setClubs(state, payload) {
         state.clubs = payload;
+    },
+    setInvoiceAgreements(state, payload) {
+        state.invoiceAgreements = payload;
     }
 };
 
@@ -23,7 +33,15 @@ export const actions = {
     fillClubs({ commit }) {
         commit('setClubs', Clubs)
     },
-    getInvoiceAgreement({}, clubId) {
-        return this.$axios.get(`/clubs/${clubId}/agreements`);
+    async getInvoiceAgreement({commit}, clubId) {
+        await this.$axios.get(`/clubs/${clubId}/agreements`).then((response) => {
+            commit('setInvoiceAgreements', response.data);
+        });
+    },
+    addInvoiceAgreement({}, invoiceAgreement) {
+        return this.$axios.post(`/clubs/${invoiceAgreement.clubId}/agreements`, invoiceAgreement);
+    },
+    updateInvoiceAgreement({}, invoice) {
+        return this.$axios.post(`/clubs/${invoice.agreement.clubId}/agreements/${invoice.id}`, invoice.agreement);
     }
 };
