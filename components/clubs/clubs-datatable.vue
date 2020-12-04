bb85264932a7aaa3a825cc81e58dfe96defa84da
Rohan van Wijk <rohan_10@live.nl>
2020-12-04 09:12:55 +0100

    Add clubs table.

---
+++ components/clubs/clubs-datatable.vue
@@ -0,0 +1,58 @@
+<template>
+    <div>
+        <v-data-table
+            :items="clubs"
+            :headers="headers"
+            :search="search"
+            :footer-props="footerPropsOptions"
+            @click:row="toClub"
+        >
+            <template v-slot:top>
+                <v-toolbar flat>
+                    <v-text-field label="Search club" v-model="search"></v-text-field>
+                </v-toolbar>
+            </template>
+        </v-data-table>
+    </div>
+</template>
+<script lang="ts">
+import { Component, Vue, Prop } from "nuxt-property-decorator";
+import {IClub} from "~/interfaces/IClub";
+
+@Component
+export default class clubsDatatableComponent extends Vue {
+    @Prop({ type: Array, required: true }) readonly clubs!: IClub[];
+    search: string = '';
+    footerPropsOptions = {
+        'items-per-page-options': [5, 10, 25, 50]
+    };
+
+    toClub(club: IClub) {
+        this.$router.push({name: 'clubs-id', params: { id: club.id.toString() }});
+    }
+
+    layout(): string {
+        return 'mws';
+    }
+
+    get headers() {
+        return [
+            {
+                text: 'Id',
+                value: 'id'
+            },
+            {
+                text: 'Club name',
+                value: 'name'
+            }
+        ];
+    }
+}
+</script>
+<style lang="scss" scoped>
+.text-start {
+    &:hover {
+        cursor: pointer;
+    }
+}
+</style>
