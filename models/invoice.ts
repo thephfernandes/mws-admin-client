b01116f3c36e50c2e4b734e5299a4cc2796418d1
Rohan van Wijk <rohan_10@live.nl>
2020-12-07 11:04:32 +0100

    changed default value to 0

--- models/invoice.ts
+++ models/invoice.ts
@@ -1,14 +1,14 @@
 export default class Invoice {
-    AgreementId: number = 1;
-    MatchId: number =  1;
-    Price: number =  1;
-    Vat: number =  1;
-    PaymentHandling: number =  1;
-    Shipping: number =  1;
-    ShippingCharge: number =  1;
-    AuctionCharge: number =  1;
-    SetupCharge: number =  1;
-    VatCharge: number =  1;
-    TotalCharge: number =  1;
-    ClubPayout: number =  1
+    AgreementId: number = 0;
+    MatchId: number =  0;
+    Price: number =  0;
+    Vat: number =  0;
+    PaymentHandling: number =  0;
+    Shipping: number =  0;
+    ShippingCharge: number =  0;
+    AuctionCharge: number =  0;
+    SetupCharge: number =  0;
+    VatCharge: number =  0;
+    TotalCharge: number =  0;
+    ClubPayout: number =  0
 }
