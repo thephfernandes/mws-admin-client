4ef31f6772dc30309d4fcb7ae558b81d962ed2a0
Rohan van Wijk <rohan_10@live.nl>
2020-12-10 11:14:32 +0100

    Data is complete and currency formatted in euro's

--- components/finance/dashboard/proceeds-datatable.vue
+++ components/finance/dashboard/proceeds-datatable.vue
@@ -95,6 +95,30 @@
                     <v-btn>Export as CSV</v-btn>
                 </v-toolbar>
             </template>
+            <template v-slot:item.date="{item}">
+                {{formatDate(item.date)}}
+            </template>
+            <template v-slot:item.setupFee="{item}">
+                &euro; {{formatCurrency(item.setupFee)}}
+            </template>
+            <template v-slot:item.vatFee="{item}">
+                &euro; {{formatCurrency(item.vatFee)}}
+            </template>
+            <template v-slot:item.handlingCosts="{item}">
+                &euro; {{formatCurrency(item.handlingCosts)}}
+            </template>
+            <template v-slot:item.totalProceeds="{item}">
+                &euro; {{formatCurrency(item.totalProceeds)}}
+            </template>
+            <template v-slot:item.avgProcPerEvent="{item}">
+                &euro; {{formatCurrency(item.avgProcPerEvent)}}
+            </template>
+            <template v-slot:item.avgMWSRevPerEvent="{item}">
+                &euro; {{formatCurrency(item.avgMWSRevPerEvent)}}
+            </template>
+            <template v-slot:item.totalMWSRev="{item}">
+                &euro; {{formatCurrency(item.totalMWSRev)}}
+            </template>
         </v-data-table>
     </div>
 </template>
@@ -107,11 +131,25 @@ export default class proceedsDatatableComponent extends Vue {
     menuEnd: boolean = false;
     dateStart: string = new Date().toISOString().substr(0, 7);
     dateEnd: string = new Date().toISOString().substr(0, 7);
+
+    formatDate(date: string): string {
+        const fullDate = new Date(date);
+        if (fullDate.toDateString() == 'Invalid Date') return date;
+        const month = fullDate.toLocaleString('default', { month: 'long' });
+        const monthCapitalized = month.charAt(0).toUpperCase() + month.slice(1);
+        return `${monthCapitalized} ${fullDate.getFullYear()}`;
+    }
+
+    formatCurrency(price: number): string {
+        return price.toFixed(2).replace('.', ',');
+    }
+
     get headers() {
         return [
             {
                 text: 'Date',
-                value: 'date'
+                value: 'date',
+                divider: true,
             },
             {
                 text: 'Setup Fee',
@@ -128,6 +166,22 @@ export default class proceedsDatatableComponent extends Vue {
             {
                 text: 'Total Proceeds',
                 value: 'totalProceeds'
+            },
+            {
+                text: 'Events',
+                value: 'numOfEvents'
+            },
+            {
+                text: 'Average proceed per event',
+                value: 'avgProcPerEvent'
+            },
+            {
+                text: 'Average MWS revenue per event',
+                value: 'avgMWSRevPerEvent'
+            },
+            {
+                text: 'Total MWS revenue',
+                value: 'totalMWSRev'
             }
         ];
     }
@@ -139,28 +193,44 @@ export default class proceedsDatatableComponent extends Vue {
                 setupFee: 21860.75,
                 vatFee: 28050.06,
                 handlingCosts: 23180.30,
-                totalProceeds: 205401.00
+                totalProceeds: 205401.00,
+                numOfEvents: 40,
+                avgProcPerEvent: 5135.03,
+                avgMWSRevPerEvent: 1247.77,
+                totalMWSRev: 49910.81
             },
             {
                 date: '2020-08-01',
                 setupFee: 7499.19,
                 vatFee: 7305.92,
                 handlingCosts: 3528.90,
-                totalProceeds: 39257.00
+                totalProceeds: 39257.00,
+                numOfEvents: 5,
+                avgProcPerEvent: 7851.40,
+                avgMWSRevPerEvent: 2961.02,
+                totalMWSRev: 14805.11
             },
             {
                 date: '2020-09-01',
                 setupFee: 18500.00,
                 vatFee: 19772.85,
                 handlingCosts: 9523.40,
-                totalProceeds: 107069.00
+                totalProceeds: 107069.00,
+                numOfEvents: 12,
+                avgProcPerEvent: 5135.03,
+                avgMWSRevPerEvent: 1247.77,
+                totalMWSRev: 49910.81
             },
             {
                 date: 'Totals',
                 setupFee: 47859.94,
                 vatFee: 55128.83,
                 handlingCosts: 36232.60,
-                totalProceeds: 351727.00
+                totalProceeds: 351727.00,
+                numOfEvents: 57,
+                avgProcPerEvent: 5135.03,
+                avgMWSRevPerEvent: 1247.77,
+                totalMWSRev: 49910.81
             }
         ];
     }
