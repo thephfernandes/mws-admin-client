f12bbb9f4a813afdebe7d32b6faaf18628c09b8e
nguyenvandung-99 <68967018+nguyenvandung-99@users.noreply.github.com>
2020-12-16 14:43:54 +0100

    add default value to invoice agreements

--- models/invoiceAgreement.ts
+++ models/invoiceAgreement.ts
@@ -1,13 +1,13 @@
 export default class InvoiceAgreement {
-    public clubId: number = 0;
-    public sellerId: number = 0;
-    public name: string = '';
-    public thirdPartyVatPercentage: number = 0;
-    public paymentHandlingPercentage: number = 0;
-    public shippingInsurancePercentage: number = 0;
-    public shippingFlatFee: number = 0;
-    public mwsHandlingPercentage: number = 0;
-    public mwsAuctionPercentage: number = 0;
-    public mwsVatPercentage: number = 0;
-    public mwsSetupFee: number = 0;
+  public clubId: number = 0;
+  public sellerId: number = 0;
+  public name: string = "Default";
+  public thirdPartyVatPercentage: string | number = "";
+  public paymentHandlingPercentage: number = 1.5;
+  public shippingInsurancePercentage: number = 1;
+  public shippingFlatFee: number = 50;
+  public mwsHandlingPercentage: number = 21;
+  public mwsAuctionPercentage: number = 21;
+  public mwsVatPercentage: number = 21;
+  public mwsSetupFee: number = 0;
 }
