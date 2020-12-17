9d79e9b5ade7d8566df3900a3ff73d3736d71ef3
nguyenvandung-99 <68967018+nguyenvandung-99@users.noreply.github.com>
2020-12-17 13:50:48 +0100

    add minimum guarantee

--- models/invoiceAgreement.ts
+++ models/invoiceAgreement.ts
@@ -6,6 +6,7 @@ export default class InvoiceAgreement {
   public paymentHandlingPercentage: number = 1.5;
   public shippingInsurancePercentage: number = 1;
   public shippingFlatFee: number = 50;
+  public minimumGuarantee: number = 0;
   public mwsHandlingPercentage: number = 21;
   public mwsAuctionPercentage: number = 21;
   public mwsVatPercentage: number = 21;
