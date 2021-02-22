4f655e4ab6c0f76a10e3793f3bf8f2e9da3930e7
pedro fernandes <thephfernandes@gmail.com>
2021-02-22 15:28:02 +0100

    initializes hardcoded products store

---
+++ store/products.js
@@ -0,0 +1,104 @@
+import axios from "axios";
+
+export const state = () => ({
+    products: [
+        {
+            productId: 1,
+            name: 'Giorgian De Arrascaeta',
+            team: 'Flamengo',
+            opposingClub: 'Internacional',
+            winningBidId: 1,
+            bids: [
+                {
+                    id: 1,
+                    amount: 2300,
+                    currencyCode: 'EUR',
+                },
+                {
+                    id: 2,
+                    amount: 2200,
+                    currencyCode: 'EUR', 
+                },
+                {
+                    id: 3,
+                    amount: 1900,
+                    currencyCode: 'EUR',
+                }
+            ]
+        },
+        {
+            productId: 2,
+            name: 'Gabriel Barbosa',
+            team: 'Flamengo',
+            opposingClub: 'Internacional time lixo',
+            winningBidId: 4,
+            bids: [
+                {
+                    id: 4,
+                    amount: 2000,
+                    currencyCode: 'EUR', 
+                },
+                {
+                    id: 5,
+                    amount: 1800, 
+                    currencyCode: 'EUR',
+                },
+                {
+                    id: 6,
+                    amount: 1700,
+                    currencyCode: 'EUR',
+                },
+            ]
+        },
+        {
+            productId: 3,
+            name: 'Everton Ribeiro',
+            team: 'Flamengo',
+            opposingClub: 'Internacional trashhh',
+            winningBidId: 8,
+            bids: [
+                {
+                    id: 7,
+                    amount: 1800,
+                    currencyCode: 'EUR',
+                },
+                {
+                    id: 8,
+                    amount: 1700,
+                    currencyCode: 'EUR',
+                }
+            ]
+        }
+    ],
+    selectedProduct: {}
+});
+
+export const getters = {
+    getProducts: (state) => {
+        return state.products;
+    },
+
+    getSelectedProduct: (state) => {
+        return state.selectedProduct;
+    }
+};
+
+export const mutations = {
+    setProducts(state, payload) {
+        state.products = payload;
+    },
+
+    setSelectedProduct(state, payload) {
+        state.selectedProduct = payload;
+    },
+
+    updateProductWinningBid(state, payload) {
+        state.selectedProduct.winningBidId = payload;
+    }
+};
+
+export const actions = {
+    async fetchProducts({ commit }, payload) {
+        //axios request and state commit
+    },
+}
