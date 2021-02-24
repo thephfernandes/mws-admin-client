87c7ddc6a9a704900351b49262154187879c09fe
pedro fernandes <thephfernandes@gmail.com>
2021-02-24 17:30:42 +0100

    removes console.logs

--- store/bids.js
+++ store/bids.js
@@ -11,20 +11,16 @@ export const getters = {
 export const mutations = {
     setBids(state, payload) {
         state.productBids = payload;
-        console.log("new state:", state.productBids)
     },
 };
 
 export const actions = {
     async fetchBids({ commit, rootGetters }) {
         const product = rootGetters["products/getSelectedProduct"];
-        console.log("selected product", product)
         await this.$axios.get(`/matches/${product.matchId}/products/${product.id}/bids`)
             .then(response => {
                 if (response.status === 200) {
-                    console.log(response)
                     const bids = response.data;
-                    console.log("response data:", bids)
                     commit("setBids", bids);
                 }
             })
